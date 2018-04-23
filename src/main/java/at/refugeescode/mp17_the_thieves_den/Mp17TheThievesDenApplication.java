998e3a2d46d30cf86a2c51da7f171dd26018ff61
Fozat Kardouh <fozatkardouh@gmail.com>
2018-04-23 16:10:55 +0200

    added empty project

---
+++ src/main/java/at/refugeescode/mp17_the_thieves_den/Mp17TheThievesDenApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp17_the_thieves_den;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp17TheThievesDenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp17TheThievesDenApplication.class, args);
+	}
+}
